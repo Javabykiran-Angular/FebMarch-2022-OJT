d6a4fdb94185baa1f37a2a6dbde33a10e09de76e
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-05-25 13:59:48 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+/*
+
+*/
+// variable 
+// var a1;
+// var a2,a3,a4;
+// var a$e;
+// var a_s;
+// var a s; it is not allowed
+//data Types
+//number(int,float,long,double),string(""/'' 'a','sumit'),boolean(true/false),
+// any(number,string,booealn,object),void(neutral data type => It does not point any thing)
+//misec. undefined & null it works as data type as well as value 
+//var obj=null;var a=undefined
+var a;
+var str;
+var a1 = 10; // Forward declaration & defination
+// console.log('Value of a1 is '+a1)
+// console.log('Value of a is '+a);
+var strany;
+strany = 2.5;
+// console.log(`
+//     value of strany ${strany}
+// `)
+strany = 'Sumit';
+// console.log(`
+//     value of strany ${strany}
+// `)
+strany = true;
+// console.log('Value of strany '+strany);
+var b = 'sumit';
+// b=2.5;
+var b1;
+console.log("Value of b1 " + b1);
