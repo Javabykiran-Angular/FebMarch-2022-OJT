e1b3e606333562d9e8dfe94e59e9ebfca1e65313
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-07-13 13:01:04 +0530

    Day23

---
+++ Angular/Day23/demotdf/demotdf.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DemotdfComponent } from './demotdf.component';
+
+describe('DemotdfComponent', () => {
+  let component: DemotdfComponent;
+  let fixture: ComponentFixture<DemotdfComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DemotdfComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DemotdfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
