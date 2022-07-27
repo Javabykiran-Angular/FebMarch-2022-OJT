ac9cb058abf3dac02fdb9696904711a2be63b405
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-07-27 12:08:47 +0530

    Day32

---
+++ Angular/Day32/shared/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+import { MatDialogModule, MatIconModule } from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [MatConfirmDialogComponent],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule
+  ],
+  exports:[
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
